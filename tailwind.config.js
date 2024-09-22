48d6207f27bd93addcc327f8184fa5213cfc3d11
Sheikh Farid <mdfaridsagor101@gmail.com>
2024-09-22 21:17:35 +0600

    added nav bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,10 @@ module.exports = {
         'Primary': '#B4F461',
         'Secondary': '#F9F7F3',
       },
+      // backgroundImage: {
+      //   'navbar': "url('./assets/logo.png')",
+      //   'footer-texture': "url('/img/footer-texture.png')",
+      // },
     },
   },
   plugins: [
